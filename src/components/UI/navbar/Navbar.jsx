f88bced0672af690ffca25277a8505c0b56fc4f5
Vlad Bas <70838774+fogirtamo@users.noreply.github.com>
2023-08-23 00:58:04 +0300

    feat: add styles for navbar

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -7,17 +7,17 @@ const Navbar = () => {
 
     return (
         <div className={cl.navbar}>
-            <h1>
+            <div className={cl.title}>
                 <Link to="/about">PetStory online</Link>
-            </h1>
-            <div>
+            </div>
+            <div className={cl.menu}>
                 <Link to="/about">About</Link>
                 <Link to="/map">Map</Link>
                 <Link to="/zoos">Zoos</Link>
                 <Link to="/donate">Donate</Link>
                 <Link to="/cantact">Contact us</Link>
             </div>
-            <div>
+            <div className={cl.designed}>
                 <Link to="https://www.figma.com/file/jfEFwkXVj1WRq7sUHDr8os/PetStory-online?type=design&node-id=0-1&mode=design&t=bIl0qssZblHrowDI-0">
                     Designed by ©
                 </Link>
