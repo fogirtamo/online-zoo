cb4b7cd0e1772d233d9f70e7a380f4840301a59e
Vlad Bas <70838774+fogirtamo@users.noreply.github.com>
2023-08-24 00:43:28 +0300

    fix: change styles

--- src/components/UI/navbar/Navbar.jsx
+++ src/components/UI/navbar/Navbar.jsx
@@ -7,16 +7,19 @@ const Navbar = () => {
 
     return (
         <div className={cl.navbar}>
-            <div className={cl.title}>
-                <Link to="/about">PetStory online</Link>
+            <div className={cl.navbar_menu}>
+                <div className={cl.title}>
+                    <Link to="/about">PetStory online</Link>
+                </div>
+                <div className={cl.menu}>
+                    <Link to="/about">About</Link>
+                    <Link to="/map">Map</Link>
+                    <Link to="/zoos">Zoos</Link>
+                    <Link to="/donate">Donate</Link>
+                    <Link to="/cantact">Contact us</Link>
+                </div>               
             </div>
-            <div className={cl.menu}>
-                <Link to="/about">About</Link>
-                <Link to="/map">Map</Link>
-                <Link to="/zoos">Zoos</Link>
-                <Link to="/donate">Donate</Link>
-                <Link to="/cantact">Contact us</Link>
-            </div>
+
             <div className={cl.designed}>
                 <Link to="https://www.figma.com/file/jfEFwkXVj1WRq7sUHDr8os/PetStory-online?type=design&node-id=0-1&mode=design&t=bIl0qssZblHrowDI-0">
                     Designed by ©
