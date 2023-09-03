f22cc936c3ff69c59e654c7f7aada86ee33a7d45
Vlad Bas <70838774+fogirtamo@users.noreply.github.com>
2023-09-04 00:26:57 +0300

    feat: add slider section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
-import Navbar from './components/UI/navbar/Navbar';
-import Panda from './components/sections/Panda';
+import React, {useEffect, useState} from 'react';
 import './styles/App.css';
 import {BrowserRouter} from "react-router-dom";
-import Backstage from './components/sections/Backstage';
+import Navbar from "./components/UI/Navbar/Navbar";
+import AppRouter from "./components/AppRouter";
+import {AuthContext} from "./context";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Panda/>
-      <Backstage/>
-    </BrowserRouter>
-    
-  );
+    const [isAuth, setIsAuth] = useState(false);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (localStorage.getItem('auth')) {
+            setIsAuth(true)
+        }
+        setLoading(false);
+    }, [])
+
+    return (
+        <AuthContext.Provider value={{
+            isAuth,
+            setIsAuth,
+            isLoading
+        }}>
+            <BrowserRouter>
+                <Navbar/>
+                <AppRouter/>
+            </BrowserRouter>
+        </AuthContext.Provider>
+    )
 }
 
 export default App;
